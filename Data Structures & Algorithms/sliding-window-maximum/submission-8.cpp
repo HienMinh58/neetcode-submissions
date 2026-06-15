ebf7b5d90903504f139ce0373adc8f405c47f3f7
HienMinh58 <151639433+HienMinh58@users.noreply.github.com>
2026-06-16 05:39:11 +1200

    Add: sliding-window-maximum - submission-8

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-8.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> q;
+        vector<int> results;
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+            if(!q.empty() && q.front() < i - k + 1){
+                q.pop_front();
+            }
+            
+            while(!q.empty() && nums[q.back()] <= nums[i]){
+                q.pop_back();
+            }
+            q.push_back(i);
+            if(i >= k - 1){
+                results.push_back(nums[q.front()]);
+            }
+        }
+        return results;
+    }
+};
