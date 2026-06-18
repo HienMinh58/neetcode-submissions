1b4cd89936dacb95635ca7dbf54d94e9846d5aa8
HienMinh58 <151639433+HienMinh58@users.noreply.github.com>
2026-06-18 12:09:18 +1200

    Add: subsets - submission-2

---
+++ Data Structures & Algorithms/subsets/submission-2.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> path;
+    vector<vector<int>> res;
+    void backtrack(vector<int>& nums, int start){
+        res.push_back(path);
+        for(int i = start; i < nums.size(); i++){
+            path.push_back(nums[i]);
+            backtrack(nums, i+1);
+            path.pop_back();
+        }
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        backtrack(nums, 0);
+        return res;
+    }
+};
